9d51bcb535112191cb0778cd44d63b7bda817c14
Roberto Alcover <bacishere@gmail.com>
2018-11-07 18:03:48 +0100

    vecinos proximos sin probar

---
+++ P1/Practica1_Pareja1/EstrategiaParticionado.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+from abc import ABCMeta,abstractmethod
+import random
+import numpy as np
+class Particion():
+
+	# Esta clase mantiene la lista de índices de Train y Test para cada partición del conjunto de particiones  
+	def __init__(self):
+		self.indicesTrain=[]
+		self.indicesTest=[]
+
+	def __init__(self,indicesTrain,indicesTest):
+		self.indicesTrain = indicesTrain
+		self.indicesTest = indicesTest
+		
+#####################################################################################################
+
+class EstrategiaParticionado(object):
+	
+	# Clase abstracta
+	__metaclass__ = ABCMeta
+	
+	# Atributos: deben rellenarse adecuadamente para cada estrategia concreta: nombreEstrategia, numeroParticiones, listaParticiones. Se pasan en el constructor 
+	def __init__(self,nombreEstrategia, numeroParticiones,seed=None):
+			self.nombreEstrategia = nombreEstrategia
+			self.numeroParticiones = numeroParticiones
+			random.seed(seed)
+	@abstractmethod
+	# TODO: esta funcion deben ser implementadas en cada estrategia concreta  
+	def creaParticiones(self,datos,seed=None):
+		pass
+	
+
+#####################################################################################################
+
+class ValidacionSimple(EstrategiaParticionado):
+
+	# Constructor que llama al de la superclase.
+	def __init__(self,numeroParticiones, porcentaje,seed=None):
+		super(ValidacionSimple,self).__init__("Validacion simple",numeroParticiones)
+		self.porcentaje = porcentaje
+	
+	# Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado.
+	# Devuelve una lista de particiones (clase Particion)
+	# TODO: implementar
+	def creaParticiones(self,datos,seed=None):
+		random.seed(seed)
+		particiones = []
+		ntot = len(datos)
+		n = int(ntot*self.porcentaje)
+		for i in range(self.numeroParticiones):
+			x = list(range(ntot))
+			random.shuffle(x)
+			p = Particion(x[:n],x[(n+1):])
+			particiones.append(p)
+		return particiones
+
+			
+			
+#####################################################################################################      
+class ValidacionCruzada(EstrategiaParticionado):
+	
+
+	def __init__(self,numFolds,datos,seed=None):
+
+		super(ValidacionCruzada,self).__init__("Validacion cruzada",numFolds,datos)
+
+
+	# Crea particiones segun el metodo de validacion cruzada.
+	# El conjunto de entrenamiento se crea con las nfolds-1 particiones y el de test con la particion restante
+	# Esta funcion devuelve una lista de particiones (clase Particion)
+	# TODO: implementar
+	def creaParticiones(self,datos,seed=None):   
+		random.seed(seed)
+		particiones = []
+		ntot = len(datos)
+		x = list(range(ntot))
+		n = int(np.floor(ntot/self.numeroParticiones))
+		r = ntot%self.numeroParticiones
+
+		for i in range((self.numeroParticiones)):
+
+			if r !=0:
+				p = Particion(x[n*i:n*(i+1)+1], x[:n*i]+x[(n*(i+1))+1:])
+				particiones.append(p)
+			else:
+				p = Particion(x[n*i:n*(i+1)], x[:n*i]+x[(n*(i+1)):])
+				particiones.append(p)
+			r = r-1
+		return particiones
+		pass
+		
+
+#####################################################################################################      
+class ValidacionBootstrap(EstrategiaParticionado):
+	
+
+	def __init__(self,numFolds,datos,seed=None):
+		super().__init__("Validacion simple",numFolds)
+	# Crea particiones segun el metodo de validacion por bootstrap.
+	# Esta funcion devuelve una lista de particiones (clase Particion)
+	# TODO: implementar
+	def creaParticiones(self,datos,seed=None):   
+		random.seed(seed)
+		l = len(datos)
+		particiones = []
+		for i in range(self.numeroParticiones):
+			p1 = list(set(np.random.randint(l,size=l)))
+			ints = [i for i in range(l) if i not in p1]
+			particiones.append(Particion(p1,ints))
+		return particiones
+		
---
+++ P1/Practica1_Pareja1/mainP1.py
@@ -0,0 +1,130 @@
+from Datos import Datos
+from EstrategiaParticionado import *
+from Clasificador import *
+import numpy as np
+from sklearn import preprocessing 
+from sklearn.naive_bayes import GaussianNB,MultinomialNB
+from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
+
+#Programa para probar el funcionamiento del codigo completo
+datasets = ["balloons.data","tic-tac-toe.data","german.data"]
+particiones = [20,10,5]
+estrategia = []
+for z in range(3):
+	print(datasets[z])
+	dataset=Datos(datasets[z])
+	#Anadimos las estrategias para comparar
+	
+	estrategia.append(ValidacionSimple(particiones[z],0.3,dataset))
+	estrategia.append(ValidacionCruzada(particiones[z],dataset))
+	estrategia.append(ValidacionBootstrap(particiones[z],dataset))
+	#Listas de errores para poder calcular estadisticos
+	erroresNV = [[],[],[]]
+	erroresL = [[],[],[]]
+	erroresSkG = [[],[],[]]
+	erroresSkN = [[],[],[]]
+	erroresSkGT = [[],[],[]]
+	erroresSkNT = [[],[],[]]
+	stdL = []
+	stdN = []
+	estrategias = ["ValidacionSimple","ValidacionCruzada","ValidacionBootstrap"]
+	print("Iteracion \tError Laplace \t Error sin Laplace \t ErrorSklearn(Validacion Simple Validacion Cruzada) (Gausian Multinomial)\n")
+	#Procesamiento de los datos para comparar con paquete sklearn
+	encAtributos = preprocessing.OneHotEncoder(
+		categorical_features = dataset.nominalAtributos[:-1],
+		sparse = False)
+	X = encAtributos.fit_transform(dataset.datos[:,:-1])
+	Y = dataset.datos[:,-1]
+	Nominal = [z for z in range(len(dataset.tipoAtributos[:-1])) if dataset.nominalAtributos[z]]
+	Continue = [z for z in range(len(dataset.nominalAtributos[:-1])) if not dataset.nominalAtributos[z]]
+
+	for i in range(3):
+		print(estrategias[i])
+		
+
+		particion = estrategia[int(i+z*3)].creaParticiones(dataset.datos)	
+		j = 0
+		for x in particion:
+			j +=1
+			cnv = ClasificadorNaiveBayes()
+			datosTrain = dataset.extraeDatos(x.indicesTrain)
+			datosTest = dataset.extraeDatos(x.indicesTest)
+
+			cnv.entrenamiento(datosTrain,dataset.nominalAtributos,dataset.diccionarios)
+			NL = cnv.clasifica(datosTest,dataset.nominalAtributos,dataset.diccionarios)
+			
+  		
+			cnvL = ClasificadorNaiveBayes()
+			cnvL.entrenamiento(datosTrain,dataset.nominalAtributos,dataset.diccionarios, True)
+
+			L = cnvL.clasifica(datosTest, dataset.nominalAtributos,dataset.diccionarios)
+			errorNV = Clasificador.error(datosTest,NL)
+			errorL = Clasificador.error(datosTest,L)
+
+			erroresNV[i].append(errorNV)
+			erroresL[i].append(errorL)
+			#Comprobamos con sklearn
+			X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=None)
+			#print(X_train)
+
+			gnb = GaussianNB()
+			y_pred = gnb.fit(X_train,y_train).predict(X_test)
+			errorSkG = (y_test != y_pred).sum()/len(y_pred)
+			erroresSkG[i].append(errorSkG)
+			gnb = MultinomialNB()
+			y_pred = gnb.fit(X_train,y_train).predict(X_test)
+			errorSkN = (y_test != y_pred).sum()/len(y_pred)
+			erroresSkN[i].append(errorSkN)
+
+			#Vemos las variaciones al tener en cuenta por separado los atributos continuos y los discretos
+			gnb = GaussianNB()
+			errorNom = 1 - np.mean(cross_val_score(gnb,X, Y,cv=10))
+			erroresSkGT[i].append(errorNom)	
+			mnb = MultinomialNB()
+			errorCon = 1- np.mean(cross_val_score(mnb,X, Y,cv=10))
+			erroresSkNT[i].append(errorCon)
+
+			print("{j}\t{l}\t{nl}\t{e}\t{e1}\t{e2}\t{e3}".format(j = j,l = errorL,nl = errorNV,e = errorSkG,e1 = errorSkN,
+																														e2 = errorNom, e3 = errorCon),end="\r")
+
+		print("----------------------------------------------------------------------------------------------------------------------------------------------------------",end="\n")
+		print("\tSegundo cuantil del error:\n")
+		#print(erroresL[i])
+		mediaL = np.median(erroresL[i])
+		mediaNv = np.median(erroresNV[i])
+		mediaSk = np.median(erroresSkG[i])
+		mediaSkN = np.median(erroresSkN[i])
+		mediaSkT = np.median(erroresSkGT[i])
+		mediaSkNT = np.median(erroresSkNT[i])
+		stdL.append(np.std(erroresL[i]))
+		stdN.append(np.std(erroresNV[i]))
+		print("\t{l}\t{nl}\t{e}\t{e1}\t{e2}\t{e3}\n".format(l = mediaL, nl = mediaNv,e = mediaSk,e1=mediaSkN,e2 = mediaSkT,e3 = mediaSkNT))
+		print("Desviaciones tipicas:\n (Con correccion de Laplace) {l} (Sin correccion de Laplace) {nl}".format(l = stdL[-1], nl =stdN[-1]))
+
+
+	n1 = np.mean(stdL)
+	n2 = np.mean(stdN)
+
+	print("Desviaciones tipicas medias::\n (Con correccion de Laplace) {l} (Sin correccion de Laplace){nl}".format(l = n1, nl = n2))
+
+
+
+#estrategia1 = ValidacionBootstrap(1,20,dataset)
+#particiones = estrategia1.creaParticiones(dataset.datos)
+#for x in particiones:
+#	print(dataset.extraeDatos(x.indicesTest))
+#	print(dataset.extraeDatos(x.indicesTrain))
+
+"""
+estrategia1 = ValidacionCruzada(3,dataset)
+particiones1 = estrategia1.creaParticiones(dataset.datos)
+for x in particiones1:
+	print ("\n")
+	print(dataset.extraeDatos(x.indicesTest))
+	print(dataset.extraeDatos(x.indicesTrain))
+"""
