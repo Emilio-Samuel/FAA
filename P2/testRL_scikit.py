1060fa80a34a24a68856e7b8ff51054a8d4e7071
Alvaro de No Fernandez <alvaro.no@estudiante.uam.es>
2018-11-14 18:41:23 +0100

    test de scikit para RL

---
+++ P2/testRL_scikit.py
@@ -0,0 +1,29 @@
+from Datos import *
+from Clasificador import *
+from EstrategiaParticionado import *
+from sklearn.linear_model import LogisticRegression
+
+datasets = ["example1.data","example2.data","example3.data","example4.data","wdbc.data"]
+n_particiones = 20
+for i in range(len(datasets)):
+    dataset = Datos(datasets[i])
+
+    particiones = ValidacionSimple(n_particiones,0.1,dataset).creaParticiones(dataset.datos)   
+    errores = np.empty(n_particiones)
+    for j in range(n_particiones):
+        datosTrain = dataset.extraeDatos(particiones[j].indicesTrain)
+        datosTest = dataset.extraeDatos(particiones[j].indicesTest)
+        cRL =  ClasificadorRegresionLineal()
+        cRL.entrenamiento(datosTrain,dataset.nominalAtributos,dataset.diccionarios,1,100)
+        res = cRL.clasifica(datosTest,dataset.nominalAtributos,dataset.diccionarios)
+
+        error = Clasificador.error(datosTest,res)
+        errores[j] = error
+    print(np.mean(errores))
+
+    cRLs = LogisticRegression(solver='lbfgs') #solver por defecto, se puede usar sag para resulatdos mas parecidos
+    cRLs.fit(datosTrain[:,:-1],datosTrain[:,-1])
+    res = cRLs.predict(datosTest[:,:-1])
+    error = Clasificador.error(datosTest,res)
+    print("Error de scikit:"),
+    print(error)
