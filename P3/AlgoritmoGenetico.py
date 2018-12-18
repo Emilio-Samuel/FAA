4ac72a5b6f1d13f3b46cf4e0456c2437211b214b
Roberto Alcover <bacishere@gmail.com>
2018-12-18 21:48:33 +0100

    cacaW

--- P3/AlgoritmoGenetico.py
+++ P3/AlgoritmoGenetico.py
@@ -1,8 +1,10 @@
 import numpy as np
 from Clasificador import *
+import time
+from scipy.stats import mode
 #SOLO 1 PARTICION!
 class ClasificadorAG(Clasificador):
-	def __init__(self,tamano_poblacion = 100,probabilidad_recombinacion=0.1,probabilidad_mutacion=0.001,proporcion_elitismo=0.05,generaciones=50, max_fitness=0.95):
+	def __init__(self,tamano_poblacion = 100,probabilidad_recombinacion=0.75,probabilidad_mutacion=0.4,proporcion_elitismo=0.05,generaciones=100, max_fitness=0.99):
 
 		self.tamano_poblacion = tamano_poblacion
 		self.proporcion_elitismo = proporcion_elitismo
@@ -10,8 +12,10 @@ class ClasificadorAG(Clasificador):
 		self.probabilidad_recombinacion = probabilidad_recombinacion
 		self.max_fitness = max_fitness
 		self.generaciones = generaciones
+		self.mejora = 0
+		self.tiempo_sin_mejora = 0
 
-	def generar_poblacion(self,nhip,cota_reglas = 5):
+	def generar_poblacion(self,nhip,cota_reglas = 50):
 		poblacion = []
 		for i in range(self.tamano_poblacion):
 			n_reglas = np.random.randint(1,cota_reglas,1)[0]
@@ -24,13 +28,21 @@ class ClasificadorAG(Clasificador):
 		aciertos = 0
 		for dato in datosTrain:
 			salida = self.discretizar_elemento(dato)
-			flag = False
+			clases = []
 			for regla in elem:
-				if((salida == regla).all() or ((salida[:-1] != regla[:-1]).any() and salida[-1] != regla[-1])):
-					flag = True
-					break
-			if flag:
-				aciertos +=1
+				flag = True
+				for i in range(len(regla)-1):
+					if regla[i]!=0 and salida[i] != regla[i]:
+						flag = False
+						break
+				if flag:
+					clases.append(regla[-1])
+				else:
+					clases.append(int(not regla[-1]))
+			#print(clases)
+			
+			if mode(clases)[0][0] == dato[-1]:
+				aciertos += 1
 				
 		return aciertos * 1./len(datosTrain)
 
@@ -48,46 +60,100 @@ class ClasificadorAG(Clasificador):
 		for i in range(self.tamano_poblacion):
 			self.fitness_poblacion[i] = self.fitness(datosTrain,self.poblacion[i])
 
-		while(self.generaciones > 0 and np.max(self.fitness_poblacion) < self.max_fitness):
+		while(self.generaciones > 0 and np.max(self.fitness_poblacion) < self.max_fitness and self.tiempo_sin_mejora < 10):
 			self.poblacion = self.seleccion_progenitores()
+			self.tamano_poblacion = len(self.poblacion)
 			for i in range(self.tamano_poblacion - 1):
-				[self.poblacion[i], self.poblacion[i+1]] = self.Cruce(self.poblacion[i], self.poblacion[i+1])
+				individuo1 = self.poblacion.pop(i)
+				individuo2 = self.poblacion.pop(i)
+				individuos = self.Cruce(individuo1,individuo2)
+				self.poblacion.append(individuos[0])
+				self.poblacion.append(individuos[1])
 				i += 1
+
 			for i in range(self.tamano_poblacion):
 				self.poblacion[i] = self.Mutacion(self.poblacion[i])
 
+			self.poblacion.extend(self.elitismo_a_mantener)
+			self.tamano_poblacion = len(self.poblacion)
 			for i in range(self.tamano_poblacion):
 				self.fitness_poblacion[i] = self.fitness(datosTrain,self.poblacion[i])
 
 			self.generaciones -= 1
+			mejora = np.max(self.fitness_poblacion)
+			if mejora > self.mejora:
+				self.tiempo_sin_mejora = 0
+				self.mejora = mejora
+			else:
+				self.tiempo_sin_mejora += 1
+			print(self.generaciones,mejora,end="\r")
 
 		n_ganador = np.argsort(self.fitness_poblacion)[-1]
-		self.regla = self.poblacion[n_ganador,:]
+		self.regla = self.poblacion[n_ganador]
 		print(self.generaciones)
 		print(self.regla)
 		print(self.fitness(datosTrain,self.regla))
 		return
 	def clasifica(self,datostest,atributosDiscretos,diccionario):
-		return 1 - self.fitness(datostest,self.regla)
-
+		clasificaciones =[]
+		for dato in datostest:
+			salida = self.discretizar_elemento(dato)
+			clases = []
+			for regla in self.regla:
+				flag = True
+				for i in range(len(regla)-1):
+					if regla[i]!=0 and salida[i] != regla[i]:
+						flag = False
+						break
+				if flag:
+					clases.append(regla[-1])
+				else:
+					clases.append(int(not regla[-1]))
+			
+			clasificaciones.append(mode(np.array(clases))[0][0]) 
+		return np.array(clasificaciones)
 	def Cruce(self,elem1, elem2):
+
+		if len(elem1.shape) == 1:
+			elem1 = elem1.reshape((1,elem1.shape[0]))
+		if len(elem2.shape) == 1:
+			elem2 = elem2.reshape((1,elem2.shape[0]))
+
 		if(np.random.rand(1)[0] > self.probabilidad_recombinacion):
 			return [elem1,elem2]
 
-		if elem1.shape[0] == elem2.shape[0] and elem.shape[0] == 1:
+		if elem2.shape[0] == 1 or elem1.shape[0] == 1:
 
-			n = np.random.randint(1,len(elem1),1)[0]
+			n = np.random.randint(1,len(elem1[0,:]),1)[0]
 
-			return [np.hstack((elem1[0:n],elem2[n:])), np.hstack((elem2[0:n],elem1[n:]))]	
+			return [np.hstack((elem1[0,0:n],elem2[0,n:])), np.hstack((elem2[0,0:n],elem1[0,n:]))]	
+		
+		else:
+
+			
+			n = np.random.randint(1,np.min((elem1.shape[0],elem2.shape[0])),1)[0]
+			
+			return [np.vstack((elem1[0:n,:],elem2[n:,:])), np.vstack((elem2[0:n,:],elem1[n:,:]))]
 		
 
 	def Mutacion(self,elem):
-		mutaciones = np.random.rand(len(elem))
-		for i in range(len(elem)-1):
-			if mutaciones[i] < self.probabilidad_mutacion:
-				elem[i] = np.random.randint(0,self.K+1,1)[0]
-		if mutaciones[-1] < self.probabilidad_mutacion:
-			elem[-1] = int(not elem[-1])
+		if len(elem.shape) == 1:
+			elem = elem.reshape((1,elem.shape[0]))
+		for fila in elem:
+			mutaciones = np.random.rand(len(fila))
+			for i in range(len(fila)-1):
+				if mutaciones[i] < self.probabilidad_mutacion:
+					fila[i] = np.random.randint(0,self.K+1,1)[0]
+			if mutaciones[-1] < self.probabilidad_mutacion:
+				fila[-1] = int(not fila[-1])
+		if np.random.rand(1)[0] < self.probabilidad_mutacion:
+			nueva_regla = np.random.randint(0,self.K+1,self.natributos)
+			nueva_regla = np.hstack((nueva_regla,np.random.randint(0,2,1)))
+			elem = np.vstack((elem,nueva_regla)) 
+		if np.random.rand(1)[0] < self.probabilidad_mutacion:
+			n = np.random.randint(1,elem.shape[0]-1,1)[0]
+			elem = np.vstack((elem[0:n],elem[n+1:]))
+
 		return elem
 
 	def discretizar_elemento(self,elem):
@@ -100,10 +166,12 @@ class ClasificadorAG(Clasificador):
 		return discretizado.astype(int)
 	def seleccion_progenitores(self):
 		elementos_mantener = int(np.floor(-1* self.proporcion_elitismo* len(self.poblacion)))
-		#print(elementos_mantener)
+		
 		posiciones = np.argsort(self.fitness_poblacion)
-		poblacion = np.copy(self.poblacion[posiciones[elementos_mantener:]])
-
+		poblacion = []
+		self.elitismo_a_mantener = []
+		for e in posiciones[elementos_mantener:]:
+			self.elitismo_a_mantener.append(self.poblacion[e])
 		probabilidades = np.cumsum(self.fitness_poblacion)/np.sum(self.fitness_poblacion)
 		probabilidades = np.hstack((0,probabilidades))
 		decision = np.random.rand(len(self.poblacion) + elementos_mantener)
@@ -111,5 +179,5 @@ class ClasificadorAG(Clasificador):
 		for p in decision:
 			#print(np.where(probabilidades < p)[0], p)
 			elegido = np.where(probabilidades < p)[0][0]
-			poblacion = np.vstack((poblacion,self.poblacion[elegido,:]))
+			poblacion.append(self.poblacion[elegido])
 		return poblacion
--- P3/test.py
+++ P3/test.py
@@ -2,11 +2,23 @@ from Datos import *
 from Clasificador import *
 from EstrategiaParticionado import *
 from AlgoritmoGenetico import *
-dataset = Datos("wdbc.data")
+from plotModel import plotModel 
+import matplotlib.pyplot as plt 
+dataset = Datos("example3.data")
 alg = ClasificadorAG()
-particiones = ValidacionBootstrap(1,0.1,dataset).creaParticiones(dataset.datos) 
+particiones = ValidacionSimple(1,0.9,dataset).creaParticiones(dataset.datos) 
 for j in range(1):
-  datosTrain = dataset.extraeDatos(particiones[j].indicesTrain)
-  datosTest = dataset.extraeDatos(particiones[j].indicesTest)
-  alg.entrenamiento(datosTrain,dataset.nominalAtributos,dataset.diccionarios)
-  print(alg.clasifica(datosTest,dataset.nominalAtributos,dataset.diccionarios))
+	datosTrain = dataset.extraeDatos(particiones[j].indicesTrain)
+	datosTest = dataset.extraeDatos(particiones[j].indicesTest)
+	alg.entrenamiento(datosTrain,dataset.nominalAtributos,dataset.diccionarios)
+	#Clasificador.error(datosTest,alg.clasifica(datosTest))
+	#"""
+	plt.figure()
+	plotModel(dataset.datos[particiones[j].indicesTrain,0],dataset.datos[particiones[j].indicesTrain,1],
+		  dataset.datos[particiones[j].indicesTrain,-1] !=0, alg,"Frontera",dataset.diccionarios)
+	plt.plot(dataset.datos[dataset.datos[:,-1]==0,0],  
+	dataset.datos[dataset.datos[:,-1]==0,1],'ro')
+	plt.plot(dataset.datos[dataset.datos[:,-1]==1,0],  
+	dataset.datos[dataset.datos[:,-1]==1,1],'bo')
+	plt.show()
+	#"""
